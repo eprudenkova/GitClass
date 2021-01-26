207ad7fb4f06b7d6d0876bf28f6653528b33706f
Evgeniya Prudenkova <evgeniya@MacBook-Pro-16.lan1>
2021-01-26 18:48:53 -0500

    more changes

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,5 +6,7 @@ public class Main {
 
         System.out.println("Hello batch 8");
         System.out.println("I am making some changes in my code");
+        System.out.println("More changes");
+
     }
 }
