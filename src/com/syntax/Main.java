a213fd0475ac910c2d9bcc8b018388fc4852b01e
Evgeniya Prudenkova <evgeniya@MacBook-Pro-16.lan1>
2021-01-26 18:23:29 -0500

    my changes

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello batch 8");
-        System.out.println("Second commit");
+        System.out.println("I am making some changes in my code");
     }
 }
